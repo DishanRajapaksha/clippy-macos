3866b3744e0d7d7f1a034b66d92d45fc09435645
DishanRajapaksha <hello@dishanrajapaksha.com>
2026-07-12 14:05:10 +0530

    Hide Clippy from full-screen spaces

--- macos/AgentWindow.swift
+++ macos/AgentWindow.swift
@@ -12,7 +12,7 @@ class AgentWindow: NSWindow {
     override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
         super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
         level = NSWindow.Level.floating
-        collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary, .stationary]
+        collectionBehavior = [.canJoinAllSpaces, .stationary]
         canHide = true
         backingType = .buffered
         isMovable = true
